74ca27615f71b61ff3d75d33fb1732246e74f285
Mohib Khan <mohibk.in@gmail.com>
2021-06-30 19:57:28 +0530

    configure: firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,18 @@
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+const auth = firebase.auth();
+
+export { firebase, FieldValue, auth };
