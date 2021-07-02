1946976837138e727c3dae2934b25f1df296ce33
Mohib Khan <mohibk.in@gmail.com>
2021-07-02 18:19:38 +0530

    feat: add not found component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { ROUTES } from "./constants";
 
 const Login = lazy(() => import("./pages/login"));
 const Signup = lazy(() => import("./pages/signup"));
+const Dashboard = lazy(() => import("./pages/dashboard"));
+const NotFound = lazy(() => import("./pages/not-found"));
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
           <Route path={ROUTES.SIGN_UP} component={Signup} />
+          <Route exact path={ROUTES.DASHBOARD} component={Dashboard} />
+          <Route path={ROUTES.NOT_FOUND} component={NotFound} />
         </Switch>
       </Suspense>
     </Router>
---
+++ src/pages/not-found.js
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+
+export default function NotFound() {
+  useEffect(() => {
+    document.title = "Not Found - Instagram";
+  }, []);
+
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center space-y-4">
+      <h1 className="text-blue-medium text-6xl font-bold">404</h1>
+      <h1 className="text-blue-medium text-4xl font-bold">Page Not Found</h1>
+    </div>
+  );
+}
