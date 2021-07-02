693ae3cc717ac61574c650c53a93b7ba9f301115
Mohib Khan <mohibk.in@gmail.com>
2021-07-02 18:07:31 +0530

    feat: add sign up route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Loader from "./components/utils/Loader";
 import { ROUTES } from "./constants";
 
 const Login = lazy(() => import("./pages/login"));
+const Signup = lazy(() => import("./pages/signup"));
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Suspense fallback={<Loader />}>
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGN_UP} component={Signup} />
         </Switch>
       </Suspense>
     </Router>
