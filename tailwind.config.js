1614410151e4cb908a21001130ade350b2964ac4
Mohib Khan <mohibk.in@gmail.com>
2021-07-02 18:06:06 +0530

    new: tailwind custom color set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,31 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
   theme: {
-    extend: {},
+    fill: (theme) => ({
+      red: theme("colors.red.primary"),
+      white: theme("colors.white"),
+    }),
+    colors: {
+      white: "#ffffff",
+      blue: {
+        medium: "#005c98",
+      },
+      black: {
+        light: "#262626",
+        faded: "#00000059",
+      },
+      gray: {
+        base: "#616161",
+        background: "#fafafa",
+        primary: "#dbdbdb",
+      },
+      red: {
+        primary: "#ed4956",
+      },
+    },
   },
   variants: {
-    extend: {},
+    display: ["group-hover"],
   },
   plugins: [],
 };
