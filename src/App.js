6430d11c61e041bc77c82999aafb9fccc39c7fc4
Mohib Khan <mohibk.in@gmail.com>
2021-10-07 21:53:13 +0530

    feat: add auth listener

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Loader from "./components/utils/Loader";
 import { ROUTES } from "./constants";
+import UserContext from "./context/user";
+import useAuthListener from "./Hooks/uesAuthListener";
 
 const Login = lazy(() => import("./pages/login"));
 const Signup = lazy(() => import("./pages/signup"));
@@ -10,17 +12,21 @@ const Dashboard = lazy(() => import("./pages/dashboard"));
 const NotFound = lazy(() => import("./pages/not-found"));
 
 function App() {
+  const { user } = useAuthListener();
+
   return (
-    <Router>
-      <Suspense fallback={<Loader />}>
-        <Switch>
-          <Route path={ROUTES.LOGIN} component={Login} />
-          <Route path={ROUTES.SIGN_UP} component={Signup} />
-          <Route exact path={ROUTES.DASHBOARD} component={Dashboard} />
-          <Route path={ROUTES.NOT_FOUND} component={NotFound} />
-        </Switch>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<Loader />}>
+          <Switch>
+            <Route path={ROUTES.LOGIN} component={Login} />
+            <Route path={ROUTES.SIGN_UP} component={Signup} />
+            <Route exact path={ROUTES.DASHBOARD} component={Dashboard} />
+            <Route path={ROUTES.NOT_FOUND} component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
