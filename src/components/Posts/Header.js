0a60b27beb6d56ce5d9e2d6a159b9239c13fc8bc
Mohib Khan <mohibk.in@gmail.com>
2021-10-09 17:41:56 +0530

    style: remove header border

--- src/components/Posts/Header.js
+++ src/components/Posts/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Header({ username, fullName }) {
   return (
-    <div className="flex border-b border-gray-primary h-4 px-4 py-5">
+    <div className="flex border-gray-primary h-4 px-4 py-5">
       <Link to={`/p/${username}`} className="flex items-center space-x-4">
         {/* <img
           src={`/images/avatars/${username}.jpg`}
