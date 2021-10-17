de2b2ffdde61ecd04391215b87df0724728e3a4e
Mohib Khan <mohibk.in@gmail.com>
2021-10-17 13:09:07 +0530

    style: fix hero image mobile style

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,6 +2,7 @@ import { useState, useContext, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
 import { ROUTES, CREDENTIALS } from "../constants";
+import Header from "../components/Header";
 
 export default function Login() {
   const history = useHistory();
@@ -43,66 +44,76 @@ export default function Login() {
   }, []);
 
   return (
-    <div className="container flex space-x-8 mx-auto max-w-screen-md items-center h-screen">
-      <div className="w-3/5">
-        <img src="/images/hero.svg" alt="A group selfie" />
-      </div>
-      <div className="flex flex-col w-full md:w-2/5">
-        <div className="flex flex-col bg-white py-2 border border-gray-primary mb-4">
-          <h1 className="flex justify-center w-full p-4">InstaQuote</h1>
-
-          {error && (
-            <p className="text-xs mb-4 text-red-primary px-4">{error}</p>
-          )}
-
-          <form
-            onSubmit={handleLogin}
-            className="flex flex-col items-center px-4"
-          >
-            <input
-              type="email"
-              aria-label="Enter email address"
-              placeholder="Email address"
-              className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
-              onChange={({ target }) => setEmail(target.value)}
-              value={email}
-            />
-
-            <input
-              type="password"
-              aria-label="Enter password"
-              placeholder="Password"
-              className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
-              onChange={({ target }) => setPassword(target.value)}
-              value={password}
-            />
-
-            <button
-              disabled={isInvalid || isLoading}
-              type="submit"
-              className={`bg-blue-medium text-white w-full rounded h-8 py-1 mb-4 ${
-                isInvalid && "opacity-50"
-              }`}
-            >
-              Login
-            </button>
-          </form>
-          <button
-            disabled={isLoading}
-            onClick={handleGuestLogin}
-            className="border w-11/12 mx-auto rounded py-0.5"
-          >
-            Guest Login
-          </button>
+    <div className="m-0 p-0">
+      <Header />
+      <div
+        className="flex flex-col md:flex-row space-x-8 w-11/12 md:mx-auto max-w-screen-md items-center"
+        style={{ height: "calc(100vh - 70px)" }}
+      >
+        <div className="mb-8 md:mb-0 md:flex md:w-3/5">
+          <img
+            src="/images/heroMobile.svg"
+            alt="post"
+            className="h-48 md:h-72"
+          />
         </div>
+        <div className="flex flex-col w-full md:w-2/5">
+          <div className="flex flex-col bg-white py-2 border border-gray-primary mb-4">
+            <h1 className="flex justify-center w-full p-4">InstaQuote</h1>
 
-        <div className="flex justify-center bg-white border border-gray-primary">
-          <p className="py-1">
-            Don't have an account?{" "}
-            <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
-              Sign Up
-            </Link>
-          </p>
+            {error && (
+              <p className="text-xs mb-4 text-red-primary px-4">{error}</p>
+            )}
+
+            <form
+              onSubmit={handleLogin}
+              className="flex flex-col items-center px-4"
+            >
+              <input
+                type="email"
+                aria-label="Enter email address"
+                placeholder="Email address"
+                className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
+                onChange={({ target }) => setEmail(target.value)}
+                value={email}
+              />
+
+              <input
+                type="password"
+                aria-label="Enter password"
+                placeholder="Password"
+                className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
+                onChange={({ target }) => setPassword(target.value)}
+                value={password}
+              />
+
+              <button
+                disabled={isInvalid || isLoading}
+                type="submit"
+                className={`bg-blue-medium text-white w-full rounded h-8 py-1 mb-4 ${
+                  isInvalid && "opacity-50"
+                }`}
+              >
+                Login
+              </button>
+            </form>
+            <button
+              disabled={isLoading}
+              onClick={handleGuestLogin}
+              className="border w-11/12 mx-auto rounded py-0.5"
+            >
+              Guest Login
+            </button>
+          </div>
+
+          <div className="flex justify-center bg-white border border-gray-primary">
+            <p className="py-1">
+              Don't have an account?{" "}
+              <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
+                Sign Up
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -3,6 +3,7 @@ import { Link, useHistory } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
 import { ROUTES } from "../constants";
 import { doesUserNameExist } from "../services/firebase";
+import Header from "../components/Header";
 
 export default function Login() {
   const history = useHistory();
@@ -60,77 +61,89 @@ export default function Login() {
   }, []);
 
   return (
-    <div className="container flex space-x-8 mx-auto max-w-screen-md items-center h-screen">
-      <div className="md:flex w-3/5">
-        <img src="/images/hero.svg" alt="a group selfie" />
-      </div>
-      <div className="flex flex-col w-full md:w-2/5">
-        <div className="flex flex-col bg-white py-2 border border-gray-primary mb-4">
-          <h1 className="flex justify-center w-full p-4 m-2">InstaQuote</h1>
-
-          {error && (
-            <p className="text-xs mb-4 text-red-primary px-4">{error}</p>
-          )}
-
-          <form
-            onSubmit={handleSignup}
-            className="flex flex-col items-center px-4"
-          >
-            <input
-              type="text"
-              aria-label="Enter username"
-              placeholder="Username"
-              className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
-              onChange={({ target }) => setUsername(target.value)}
-              value={username}
-            />
-
-            <input
-              type="text"
-              aria-label="Enter Full Name"
-              placeholder="Full Name"
-              className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
-              onChange={({ target }) => setFullName(target.value)}
-              value={fullName}
-            />
-
-            <input
-              type="email"
-              aria-label="Enter email address"
-              placeholder="Email address"
-              className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
-              onChange={({ target }) => setEmail(target.value)}
-              value={email}
-            />
-
-            <input
-              type="password"
-              aria-label="Enter password"
-              placeholder="Password"
-              className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
-              onChange={({ target }) => setPassword(target.value)}
-              value={password}
-            />
-
-            <button
-              disabled={isInvalid}
-              type="submit"
-              className={`bg-blue-medium text-white w-full rounded py-1 mb-4 ${
-                isInvalid && "opacity-50"
-              }`}
-            >
-              Signup
-            </button>
-          </form>
+    <div className="m-0 p-0">
+      <Header />
+      <div
+        className="flex flex-col md:flex-row space-x-8 w-11/12 md:mx-auto max-w-screen-md items-center"
+        style={{ height: "calc(100vh - 70px)" }}
+      >
+        <div className="mb-8 md:mb-0 md:flex md:w-3/5">
+          <img
+            src="/images/heroMobile.svg"
+            alt="post"
+            className="h-48 md:h-72"
+          />
         </div>
+        <div className="flex flex-col w-full mx-auto md:w-2/5">
+          <div className="flex flex-col bg-white py-2 border border-gray-primary mb-4">
+            <h1 className="flex justify-center w-full p-4 m-2 font-semibold text-xl">
+              InstaQuote
+            </h1>
 
-        <div className="flex justify-center bg-white border border-gray-primary">
-          <p className="py-1">
-            Already have an account?{" "}
-            <Link to={ROUTES.LOGIN} className="font-bold text-blue-medium">
-              Login
-            </Link>
-          </p>
+            {error && (
+              <p className="text-xs mb-4 text-red-primary px-4">{error}</p>
+            )}
+
+            <form
+              onSubmit={handleSignup}
+              className="flex flex-col items-center px-4"
+            >
+              <input
+                type="text"
+                aria-label="Enter username"
+                placeholder="Username"
+                className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
+                onChange={({ target }) => setUsername(target.value)}
+                value={username}
+              />
+
+              <input
+                type="text"
+                aria-label="Enter Full Name"
+                placeholder="Full Name"
+                className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
+                onChange={({ target }) => setFullName(target.value)}
+                value={fullName}
+              />
+
+              <input
+                type="email"
+                aria-label="Enter email address"
+                placeholder="Email address"
+                className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
+                onChange={({ target }) => setEmail(target.value)}
+                value={email}
+              />
+
+              <input
+                type="password"
+                aria-label="Enter password"
+                placeholder="Password"
+                className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
+                onChange={({ target }) => setPassword(target.value)}
+                value={password}
+              />
+
+              <button
+                disabled={isInvalid}
+                type="submit"
+                className={`bg-blue-medium text-white w-full rounded py-1 mb-4 ${
+                  isInvalid && "opacity-50"
+                }`}
+              >
+                Signup
+              </button>
+            </form>
+          </div>
+
+          <div className="flex justify-center bg-white border border-gray-primary">
+            <p className="py-1">
+              Already have an account?{" "}
+              <Link to={ROUTES.LOGIN} className="font-bold text-blue-medium">
+                Login
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
     </div>
