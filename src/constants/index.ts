135e9439759199ad09d35c00439b3328fad2c069
Mohib Khan <mohibk.in@gmail.com>
2021-11-21 22:12:41 +0530

    feat: integrate Formik & Yup

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
+import * as Yup from "yup";
+
 export const ROUTES = {
   DASHBOARD: "/",
   LOGIN: "/login",
@@ -9,4 +11,20 @@ export const ROUTES = {
 export const CREDENTIALS = {
   email: "mohibk.in@gmail.com",
   password: "falconnine",
+};
+
+export const signUpValidationSchema = Yup.object({
+  username: Yup.string().required("Username is missing"),
+  fullName: Yup.string().required("Fullname is missing"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is missing"),
+  password: Yup.string().required("Password is missing"),
+});
+
+export const signUpInitialValues = {
+  username: "",
+  fullName: "",
+  email: "",
+  password: "",
 };
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,9 +1,22 @@
 import { useState, useContext, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
-import FirebaseContext from "../context/firebase";
-import { ROUTES } from "../constants";
-import { doesUserNameExist } from "../services/firebase";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+// Components
 import Header from "../components/Header";
+import TextError from "../components/utils/TextError";
+
+// Context
+import FirebaseContext from "../context/firebase";
+
+// Utilities
+import { doesUserNameExist } from "../services/firebase";
+
+import {
+  ROUTES,
+  signUpInitialValues,
+  signUpValidationSchema,
+} from "../constants";
 
 const Login: React.FC = () => {
   const history = useHistory();
@@ -15,8 +28,12 @@ const Login: React.FC = () => {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const isInvalid = password === "" || email === "";
+  const [, setError] = useState("");
+
+  const onSubmit = (values: any) => {
+    console.log("values");
+    console.log(values);
+  };
 
   const handleSignup = async (event: { preventDefault: () => void }) => {
     event.preventDefault();
@@ -82,60 +99,69 @@ const Login: React.FC = () => {
               InstaQuote
             </h1>
 
-            {error && (
-              <p className="text-xs mb-4 text-red-primary px-4">{error}</p>
-            )}
-
-            <form
-              onSubmit={handleSignup}
-              className="flex flex-col items-center px-4"
+            <Formik
+              initialValues={signUpInitialValues}
+              onSubmit={onSubmit}
+              validationSchema={signUpValidationSchema}
             >
-              <input
-                type="text"
-                aria-label="Enter username"
-                placeholder="Username"
-                className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
-                onChange={({ target }) => setUsername(target.value)}
-                value={username}
-              />
-
-              <input
-                type="text"
-                aria-label="Enter Full Name"
-                placeholder="Full Name"
-                className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
-                onChange={({ target }) => setFullName(target.value)}
-                value={fullName}
-              />
-
-              <input
-                type="email"
-                aria-label="Enter email address"
-                placeholder="Email address"
-                className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
-                onChange={({ target }) => setEmail(target.value)}
-                value={email}
-              />
-
-              <input
-                type="password"
-                aria-label="Enter password"
-                placeholder="Password"
-                className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
-                onChange={({ target }) => setPassword(target.value)}
-                value={password}
-              />
-
-              <button
-                disabled={isInvalid}
-                type="submit"
-                className={`bg-blue-medium text-white w-full rounded py-1 mb-4 ${
-                  isInvalid && "opacity-50"
-                }`}
+              <Form
+                // onSubmit={handleSignup}
+                className="flex flex-col items-center px-4"
               >
-                Signup
-              </button>
-            </form>
+                <div className="w-full">
+                  <Field
+                    type="text"
+                    name="username"
+                    aria-label="Enter username"
+                    placeholder="Username"
+                    className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
+                  />
+                  {/* @ts-ignore TODO: fix type */}
+                  <ErrorMessage name="username" component={TextError} />
+                </div>
+
+                <div className="w-full">
+                  <Field
+                    type="text"
+                    name="fullName"
+                    placeholder="Full Name"
+                    className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
+                  />
+                  {/* @ts-ignore */}
+                  <ErrorMessage name="fullName" component={TextError} />
+                </div>
+
+                <div className="w-full">
+                  <Field
+                    type="email"
+                    name="email"
+                    placeholder="Email address"
+                    className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
+                  />
+                  {/* @ts-ignore */}
+                  <ErrorMessage name="email" component={TextError} />
+                </div>
+
+                <div className="w-full">
+                  <Field
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
+                  />
+                  {/* @ts-ignore */}
+                  <ErrorMessage name="password" component={TextError} />
+                </div>
+
+                <button
+                  // disabled={isInvalid}
+                  type="submit"
+                  className={`bg-blue-medium text-white w-full rounded py-1 mb-4`}
+                >
+                  Signup
+                </button>
+              </Form>
+            </Formik>
           </div>
 
           <div className="flex justify-center bg-white border border-gray-primary">
