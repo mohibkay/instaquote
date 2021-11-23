38512aaabf0297b4aa5aea1bb9e26fe9874d3599
Mohib Khan <mohibk.in@gmail.com>
2021-11-23 22:30:14 +0530

    feat: add API call in formik onsubmit

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -24,19 +24,22 @@ const Login: React.FC = () => {
 
   const { firebase } = useContext(FirebaseContext);
 
-  const [username, setUsername] = useState("");
-  const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [, setError] = useState("");
+  // const [username, setUsername] = useState("");
+  // const [fullName, setFullName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  const onSubmit = (values: any) => {
-    console.log("values");
-    console.log(values);
-  };
+  // const onSubmit = (values: any) => {
+  //   console.log("values");
+  //   console.log(values);
+  // };
 
-  const handleSignup = async (event: { preventDefault: () => void }) => {
-    event.preventDefault();
+  const onSubmit = async (values: any, actions: any) => {
+    console.log("actions");
+    console.log(actions);
+    const { username, email, password, fullName } = values;
+    setError("");
     const usernameExists = await doesUserNameExist(username);
 
     if (!usernameExists.length) {
@@ -64,11 +67,10 @@ const Login: React.FC = () => {
 
         history.push(ROUTES.DASHBOARD);
       } catch (error: any) {
-        setUsername("");
-        setFullName("");
-        setEmail("");
-        setPassword("");
         setError(error.message);
+      } finally {
+        actions.setSubmitting(false);
+        actions.resetForm();
       }
     } else {
       setError("Username already taken! Please try another.");
@@ -99,68 +101,80 @@ const Login: React.FC = () => {
               InstaQuote
             </h1>
 
+            {error && (
+              <p className="text-xs mb-4 text-red-primary px-4">{error}</p>
+            )}
+
             <Formik
               initialValues={signUpInitialValues}
               onSubmit={onSubmit}
               validationSchema={signUpValidationSchema}
             >
-              <Form
-                // onSubmit={handleSignup}
-                className="flex flex-col items-center px-4"
-              >
-                <div className="w-full">
-                  <Field
-                    type="text"
-                    name="username"
-                    aria-label="Enter username"
-                    placeholder="Username"
-                    className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
-                  />
-                  {/* @ts-ignore TODO: fix type */}
-                  <ErrorMessage name="username" component={TextError} />
-                </div>
+              {(formik) => {
+                console.log("formik");
+                console.log(formik);
+                return (
+                  <Form
+                    // onSubmit={handleSignup}
+                    className="flex flex-col items-center px-4"
+                  >
+                    <div className="w-full">
+                      <Field
+                        type="text"
+                        name="username"
+                        aria-label="Enter username"
+                        placeholder="Username"
+                        className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
+                      />
+                      {/* @ts-ignore TODO: fix type */}
+                      <ErrorMessage name="username" component={TextError} />
+                    </div>
 
-                <div className="w-full">
-                  <Field
-                    type="text"
-                    name="fullName"
-                    placeholder="Full Name"
-                    className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
-                  />
-                  {/* @ts-ignore */}
-                  <ErrorMessage name="fullName" component={TextError} />
-                </div>
+                    <div className="w-full">
+                      <Field
+                        type="text"
+                        name="fullName"
+                        placeholder="Full Name"
+                        className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
+                      />
+                      {/* @ts-ignore */}
+                      <ErrorMessage name="fullName" component={TextError} />
+                    </div>
 
-                <div className="w-full">
-                  <Field
-                    type="email"
-                    name="email"
-                    placeholder="Email address"
-                    className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
-                  />
-                  {/* @ts-ignore */}
-                  <ErrorMessage name="email" component={TextError} />
-                </div>
+                    <div className="w-full">
+                      <Field
+                        type="email"
+                        name="email"
+                        placeholder="Email address"
+                        className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
+                      />
+                      {/* @ts-ignore */}
+                      <ErrorMessage name="email" component={TextError} />
+                    </div>
 
-                <div className="w-full">
-                  <Field
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
-                  />
-                  {/* @ts-ignore */}
-                  <ErrorMessage name="password" component={TextError} />
-                </div>
+                    <div className="w-full">
+                      <Field
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        className="px-2 py-0.5 mb-2 border w-full border-gray-primary focus:outline-none rounded"
+                      />
+                      {/* @ts-ignore */}
+                      <ErrorMessage name="password" component={TextError} />
+                    </div>
 
-                <button
-                  // disabled={isInvalid}
-                  type="submit"
-                  className={`bg-blue-medium text-white w-full rounded py-1 mb-4`}
-                >
-                  Signup
-                </button>
-              </Form>
+                    <button
+                      // disabled={isInvalid}
+                      type="submit"
+                      className={`bg-blue-medium text-white w-full rounded py-1 mb-4 ${
+                        formik.isSubmitting && "opacity-75 cursor-not-allowed"
+                      }`}
+                    >
+                      {formik.isSubmitting ? "Signing up..." : "Signup"}
+                    </button>
+                  </Form>
+                );
+              }}
             </Formik>
           </div>
 
